90ed2d156b50d92a605683dcb4bca867e9a93c2b
KALAISELVAN16012002 <kalaitechcse37@gmail.com>
2024-02-17 11:50:41 +0530

    new

---
+++ scroll.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const navbar = document.getElementById('navbar-default');
+    toggleButton.addEventListener('click', function () {
+        navbar.classList.toggle('hidden');
+    });
+});
+// Scroll to Contact section
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector('#contact').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+    // Scroll to Reserve now section
+    document.querySelector('a[href="#reservenow"]').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector('#reservenow').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+    // Scroll to About Us section
+    document.querySelector('a[href="#aboutus"]').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector('#aboutus').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+    // Scroll to menu section
+    document.querySelector('a[href="#menu"]').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector('#menu').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+function shakeOnScroll() {
+const imagesToShake = document.querySelectorAll('.shake-on-scroll');
+imagesToShake.forEach(image => {
+const bounding = image.getBoundingClientRect();
+if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+) {
+    image.classList.add('shake');
+} else {
+    image.classList.remove('shake');
+}
+});
+}
+window.addEventListener('scroll', shakeOnScroll);
