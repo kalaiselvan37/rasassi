b06c8c92e06915a8aaadaa5d73b9550dbc3a97fe
KALAISELVAN16012002 <kalaitechcse37@gmail.com>
2024-02-19 16:37:03 +0530

    new

--- scroll.js
+++ scroll.js
@@ -1,10 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById('toggleButton');
-    const navbar = document.getElementById('navbar-default');
-    toggleButton.addEventListener('click', function () {
-        navbar.classList.toggle('hidden');
-    });
-});
+
 // Scroll to Contact section
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
@@ -41,6 +35,30 @@ document.addEventListener("DOMContentLoaded", function () {
             behavior: 'smooth'
         });
     });
+});
+function shakeOnScroll() {
+const imagesToShake = document.querySelectorAll('.shake-on-scroll');
+imagesToShake.forEach(image => {
+const bounding = image.getBoundingClientRect();
+if (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+) {
+    image.classList.add('shake');
+} else {
+    image.classList.remove('shake');
+}
+});
+}
+window.addEventListener('scroll', shakeOnScroll);
+document.addEventListener("DOMContentLoaded", function() {
+    var currentUrl = window.location.pathname;
+
+    if (currentUrl === '#menu') {
+        document.getElementById('menu').classList.add('text-red-800', 'md:text-xl');
+    }
 });
 
 const buttons = document.querySelectorAll('a');
