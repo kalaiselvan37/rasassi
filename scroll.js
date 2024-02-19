b8aeee0b141dc23c4dd73f785ce5dc7dc381132a
KALAISELVAN16012002 <kalaitechcse37@gmail.com>
2024-02-19 11:03:27 +0530

    new

--- scroll.js
+++ scroll.js
@@ -7,35 +7,35 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 // Scroll to Contact section
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
+    document.querySelector('a[href="/#contact"]').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#contact').scrollIntoView({
             behavior: 'smooth'
         });
     });
     // Scroll to Home section
-    document.querySelector('a[href="#home"]').addEventListener('click', function (e) {
+    document.querySelector('a[href="/#home"]').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#home').scrollIntoView({
             behavior: 'smooth'
         });
     });
     // Scroll to Reserve now section
-    document.querySelector('a[href="#reservenow"]').addEventListener('click', function (e) {
+    document.querySelector('a[href="/#reservenow"]').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#reservenow').scrollIntoView({
             behavior: 'smooth'
         });
     });
     // Scroll to About Us section
-    document.querySelector('a[href="#aboutus"]').addEventListener('click', function (e) {
+    document.querySelector('a[href="/#aboutus"]').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#aboutus').scrollIntoView({
             behavior: 'smooth'
         });
     });
     // Scroll to menu section
-    document.querySelector('a[href="#menu"]').addEventListener('click', function (e) {
+    document.querySelector('a[href="/#menu"]').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#menu').scrollIntoView({
             behavior: 'smooth'
